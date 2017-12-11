8df1b9f44a1f128c26c9d41158e03d0083eb587c
lovit <soy.lovit@gmail.com>
2017-12-11 20:17:34 +0900

    Add character_is_korean ftn

--- fast_hangle_levenshtein/_hangle.py
+++ fast_hangle_levenshtein/_hangle.py
@@ -1,14 +1,11 @@
 import re
 import numpy as np
 
-kor_begin     = 44032
-kor_end       = 55203
+kor_begin, kor_end = (44032, 55203)
+jaum_begin, jaum_end = (912593, 12622)
+moum_begin, moum_end = (12623, 12643)
 chosung_base  = 588
 jungsung_base = 28
-jaum_begin = 12593
-jaum_end = 12622
-moum_begin = 12623
-moum_end = 12643
 
 chosung_list = [ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 
         'ㅅ', 'ㅆ', 'ㅇ' , 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
@@ -35,10 +32,10 @@ moum_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ
 def compose(chosung, jungsung, jongsung):
     return chr(kor_begin + chosung_base * chosung_list.index(chosung) + jungsung_base * jungsung_list.index(jungsung) + jongsung_list.index(jongsung))
 
-def decompose(c):
+def decompose(c):    
     if not character_is_korean(c):
         return None
-    i = to_base(c)
+    i = ord(c)
     if (jaum_begin <= i <= jaum_end):
         return (c, ' ', ' ')
     if (moum_begin <= i <= moum_end):
@@ -48,3 +45,7 @@ def decompose(c):
     jung = ( i - cho * chosung_base ) // jungsung_base 
     jong = ( i - cho * chosung_base - jung * jungsung_base )    
     return (chosung_list[cho], jungsung_list[jung], jongsung_list[jong])
+
+def character_is_korean(c):
+    i = ord(c)
+    return (kor_begin <= i <= kor_end) or (jaum_begin <= i <= jaum_end) or (moum_begin <= i <= moum_end)
