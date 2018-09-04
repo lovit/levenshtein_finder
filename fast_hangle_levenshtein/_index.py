e03eaee330b83d64cc3dd1c312af618c2552a618
lovit <soy.lovit@gmail.com>
2018-09-04 22:02:43 +0900

    Change variable name more clearly

--- fast_hangle_levenshtein/_index.py
+++ fast_hangle_levenshtein/_index.py
@@ -6,23 +6,23 @@ from ._hangle import decompose
 from ._hangle import character_is_korean
 
 class LevenshteinIndex:
-    def __init__(self, word_counter=None, levenshtein_distance=levenshtein, verbose=False):
+    def __init__(self, words=None, levenshtein_distance=levenshtein, verbose=False):
         self._words = {}
         self._index = {} # character to words
         self._cho_index, self._jung_index, self._jong_index = {}, {}, {}
-        if word_counter:
-            self.indexing(word_counter)
+        if words:
+            self.indexing(words)
         self.verbose = verbose
         self.jamo_score = 1/3
         
-    def indexing(self, word_counter):
-        self._words = word_counter if type(word_counter) == dict else {w:1 for w in word_counter if w}
+    def indexing(self, words):
+        self._words = words if type(words) == dict else {w:0 for w in words if w}
         self._index = defaultdict(lambda: set())
         self._cho_index = defaultdict(lambda: set())
         self._jung_index = defaultdict(lambda: set())
         self._jong_index = defaultdict(lambda: set())
         
-        for word in word_counter:
+        for word in words:
             # Indexing for levenshtein
             for c in word:
                 self._index[c].add(word)
@@ -40,24 +40,25 @@ class LevenshteinIndex:
         self._jung_index = dict(self._jung_index)
         self._jong_index = dict(self._jong_index)
     
-    def levenshtein_search(self, word, max_distance=1):
+    def levenshtein_search(self, query, max_distance=1):
         search_time = time()
-        candidates = defaultdict(int)
-        (n, nc) = (len(word), len(set(word)))
-        for c in set(word):
+        similars = defaultdict(int)
+        (n, nc) = (len(query), len(set(query)))
+        for c in set(query):
             for item in self._index.get(c, {}):
-                candidates[item] += 1
+                similars[item] += 1
                 
         if self.verbose:
-            print('query={}, candidates={} '.format(word, len(candidates)), end='')
+            print('query={}, candidates={} '.format(query, len(similars)), end='')
             
-        candidates = {c for c,f in candidates.items() if (abs(n-len(c)) <= max_distance) and (abs(nc - f) <= max_distance)}
+        similars = {word for word,f in similars.items()
+                    if (abs(n-len(word)) <= max_distance) and (abs(nc - f) <= max_distance)}
         if self.verbose:
-            print('-> {}'.format(len(candidates)), end='')
+            print('-> {}'.format(len(similars)), end='')
         
         dist = {}
-        for c in candidates:
-            dist[c] = levenshtein(c, word)
+        for word in similars:
+            dist[word] = levenshtein(word, query)
         
         if self.verbose:
             search_time = time() - search_time
@@ -65,33 +66,34 @@ class LevenshteinIndex:
         
         return sorted(filter(lambda x:x[1] <= max_distance, dist.items()), key=lambda x:x[1])
     
-    def jamo_levenshtein_search(self, word, max_distance=1):
+    def jamo_levenshtein_search(self, query, max_distance=1):
         search_time = time()
-        candidates = defaultdict(lambda: 0)
-        (n, nc) = (len(word), len(set(word)))
-        for c in set(word):
+        similars = defaultdict(lambda: 0)
+        (n, nc) = (len(query), len(set(query)))
+        for c in set(query):
             if not character_is_korean(c):
                 for item in self._index.get(c, {}):
-                    candidates[item] += 1
+                    similars[item] += 1
                 continue
             cho, jung, jong = decompose(c)
             for item in self._cho_index.get(cho, {}):
-                candidates[item] += self.jamo_score
+                similars[item] += self.jamo_score
             for item in self._jung_index.get(jung, {}):
-                candidates[item] += self.jamo_score
+                similars[item] += self.jamo_score
             for item in self._jong_index.get(jong, {}):
-                candidates[item] += self.jamo_score
+                similars[item] += self.jamo_score
                 
         if self.verbose:
-            print('query={}, candidates={} '.format(word, len(candidates)), end='')
+            print('query={}, candidates={} '.format(query, len(similars)), end='')
             
-        candidates = {c for c,f in candidates.items() if (abs(n-len(c)) <= max_distance) and (abs(nc - f) <= max_distance)}
+        similars = {word for word,f in similars.items()
+                    if (abs(n-len(word)) <= max_distance) and (abs(nc - f) <= max_distance)}
         if self.verbose:
-            print('-> {}'.format(len(candidates)), end='')
+            print('-> {}'.format(len(similars)), end='')
         
         dist = {}
-        for c in candidates:
-            dist[c] = jamo_levenshtein(c, word)
+        for word in similars:
+            dist[word] = jamo_levenshtein(word, query)
         
         if self.verbose:
             search_time = time() - search_time
